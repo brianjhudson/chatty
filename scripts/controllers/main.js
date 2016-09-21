0796850308a850d9be6618309d1e561e90b94fc2
Brian Hudson <brianjasonhudson@gmail.com>
2016-09-21 15:44:25 -0500

    "Added userprofile and photo"

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -2,6 +2,7 @@
 
 angular.module('chattyApp')
   .controller('MainCtrl', function ( $scope, messageService ) {
+    $scope.currentUser = {};
     messageService.getMessages().then(function ( response ) {
       $scope.messages = response.data;
     });
@@ -10,9 +11,18 @@ angular.module('chattyApp')
       if (message) {
         messageService.addMessage(message).then(function ( response ) {
           $scope.messages = response.data;
+          $scope.newMessage = "";
         });
       }
     };
+
+    $scope.addUser = function ( user ) {
+      if (user) {
+        $scope.currentUser = user;
+        $scope.user.username = "";
+        $scope.user.profileUrl = "";
+      }
+    }
 
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
