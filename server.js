bfc1c6b78a1fb4fdb4759b22da6e6ff6710aeff4
Brian Hudson <brianjasonhudson@gmail.com>
2016-09-21 13:17:01 -0500

    "Initial commit; configured data and get method"

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
+const express = require( "express");
+const { json } = require( "body-parser" );
 
+const app = express();
+const port = 5000;
+
+app.use( json() );
+
+const messages = [
+  {
+    _id: 1
+    , content: "Hi there"
+  }
+];
+
+app.get('/api/messages', ( req, res ) => {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).json( messages );
+  // res.json(messages)
+});
+
+
+
+
+
+app.listen( port, () => {
+  console.log(`Listening on port ${ port }`);
+})
